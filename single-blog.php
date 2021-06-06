5191997cf36061e5c692ae2aa343bfdcdbec4953
PatrickFarley <patrickfarley.work@gmail.com>
2021-06-06 19:51:36 -0400

    move prev and next links

--- single-blog.php
+++ single-blog.php
@@ -12,14 +12,16 @@ get_header("blog"); ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
+
+
+					<?php get_template_part( 'content-single', get_post_format() ); ?>
+
 					<nav id="nav-single">
 						<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>
 						<span class="nav-previous"><?php previous_post_link( '%link', __( '<span class="meta-nav">&larr;</span> Previous', 'twentyeleven' ) ); ?></span>
 						<span class="nav-next"><?php next_post_link( '%link', __( 'Next <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></span>
 					</nav><!-- #nav-single -->
-
-					<?php get_template_part( 'content-single', get_post_format() ); ?>
-
+					
 					<?php comments_template( '', true ); ?>
 
 				<?php endwhile; // end of the loop. ?>
