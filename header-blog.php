2010df7b43bc55cf0d9d6555410ed2454a1fdf61
PatrickFarley <patrickfarley.work@gmail.com>
2023-02-20 16:02:29 -0500

    rm redundant site name appendage

--- header-blog.php
+++ header-blog.php
@@ -30,9 +30,6 @@
 
 	wp_title( '|', true, 'right' );
 
-	// Add the blog name.
-	bloginfo( 'name' );
-
 	// Add the blog description for the home/front page.
 	$site_description = get_bloginfo( 'description', 'display' );
 	if ( $site_description && ( is_home() || is_front_page() ) )
